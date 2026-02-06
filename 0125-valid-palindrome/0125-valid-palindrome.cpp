c2754c62ea6cd7e312a0bedf55c41d9122155e11
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-02-06 19:33:11 +0530

    Time: 1 ms (46.09%), Space: 10.8 MB (5.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str="";
+        string res="";
         for(auto i:s){
-            if((i>='a' && i<='z') || (i>='A' && i<='Z')){
-                if(isalnum(i)){
-                str+=tolower(i);
-                }
+            if(isalnum(i)){
+                res+=tolower(i);
             }
         }
-        
-        string res1=str;
-        reverse(str.begin(),str.end());
-        return str==res1;
+        string str=res;
+        reverse(res.begin(),res.end());
+        return res==str;
+
+
     }
 };
