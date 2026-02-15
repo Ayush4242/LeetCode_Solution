1f7c0eb0cad2e5165d40bbac97015964b1819140
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-02-15 16:13:05 +0530

    Time: 21 ms (47.01%), Space: 38.6 MB (42.65%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,36 +4,35 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        vector<vector<int>>dist(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0 && mat[i][j]==0){
-                    vis[i][j]=1;
+                if(mat[i][j]==0){
                     q.push({{i,j},0});
-                }
-                else{
-                    vis[i][j]=0;
+                    vis[i][j]=1;
                 }
             }
         }
         int delrow[]={-1,0,1,0};
         int delcol[]={0,1,0,-1};
+        int time=0;
         while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int d=q.front().second;
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int t=q.front().second;
             q.pop();
-            dist[r][c]=d;
+            time=max(time,t);
             for(int i=0;i<4;i++){
-                int nrow=r+delrow[i];
-                int ncol=c+delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && mat[nrow][ncol]==1){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m  && !vis[nrow][ncol]){
+                    q.push({{nrow,ncol},t+1});
                     vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},d+1});
+                    mat[nrow][ncol]=t+1;
                 }
+                
             }
         }
-        return dist;
+        return mat;
     }
 };
