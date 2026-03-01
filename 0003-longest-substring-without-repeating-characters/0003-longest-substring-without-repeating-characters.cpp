a1b7a2342addf2b011d3186165613180710a7a40
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-03-01 15:11:31 +0530

    Time: 17 ms (31.11%), Space: 12.6 MB (37.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,10 +7,14 @@ public:
             mp[s[j]]++;
             while(mp[s[j]]>1){
                 mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
+                }
                 i++;
             }
             maxi=max(maxi,j-i+1);
             j++;
+            
         }
         return maxi;
     }
