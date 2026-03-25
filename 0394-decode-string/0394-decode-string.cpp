091377ed8db6f4bc073a329da3a16f438daa5638
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-03-25 17:17:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -2,50 +2,39 @@ class Solution {
 public:
     string decodeString(string s) {
         stack<char> st;
-
-        for (char c : s) {
-            if (c != ']') {
-                st.push(c);
-            } 
-            else {
-                string curr = "";
-
-                // get string inside []
-                while (!st.empty() && st.top() != '[') {
-                    curr += st.top();
+        for(auto i:s){
+            if(i!=']'){
+                st.push(i);
+            }
+            else{
+                string curr="";
+                while(!st.empty() && st.top()!='['){
+                    curr+=st.top();
                     st.pop();
                 }
-                reverse(curr.begin(), curr.end());
-
-                st.pop(); // remove '['
-
-                // get number
-                string num = "";
-                while (!st.empty() && isdigit(st.top())) {
-                    num += st.top();
+                st.pop();
+                reverse(curr.begin(),curr.end());
+                string num="";
+                while(!st.empty() && isdigit(st.top())){
+                    num+=st.top();
                     st.pop();
                 }
-                reverse(num.begin(), num.end());
-
-                int k = stoi(num);
-
-                // push repeated string back
-                while (k--) {
-                    for (char ch : curr) {
-                        st.push(ch);
+                reverse(num.begin(),num.end());
+                int k=stoi(num);
+                while(k--){
+                    for(auto i:curr){
+                        st.push(i);
+                        // k--;
                     }
                 }
             }
         }
-
-        // build result
-        string res = "";
-        while (!st.empty()) {
-            res += st.top();
+        string res="";
+        while(!st.empty()){
+            res+=st.top();
             st.pop();
         }
-        reverse(res.begin(), res.end());
-
+        reverse(res.begin(),res.end());
         return res;
     }
 };
