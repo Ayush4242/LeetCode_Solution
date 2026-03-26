001e756386b84732c4ea7b448c5543dc3c33d44e
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-03-26 19:39:26 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.09%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<char> st;
+        string ans="";
+        stack<char>st;
         for(auto i:s){
             if(i!=']'){
                 st.push(i);
             }
             else{
-                string curr="";
+                string ans="";
                 while(!st.empty() && st.top()!='['){
-                    curr+=st.top();
+                    ans+=st.top();
                     st.pop();
                 }
                 st.pop();
-                reverse(curr.begin(),curr.end());
+                reverse(ans.begin(),ans.end());
                 string num="";
                 while(!st.empty() && isdigit(st.top())){
                     num+=st.top();
@@ -22,19 +23,20 @@ public:
                 reverse(num.begin(),num.end());
                 int k=stoi(num);
                 while(k--){
-                    for(auto i:curr){
+                    for(auto i:ans){
                         st.push(i);
-                        // k--;
                     }
                 }
             }
+            
+            
         }
         string res="";
-        while(!st.empty()){
-            res+=st.top();
-            st.pop();
-        }
-        reverse(res.begin(),res.end());
+            while(!st.empty()){
+                res+=st.top();
+                st.pop();
+            }
+            reverse(res.begin(),res.end());
         return res;
     }
 };
