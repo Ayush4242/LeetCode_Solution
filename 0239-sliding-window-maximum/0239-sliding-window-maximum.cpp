349c7e96fe709b3093f41979a0073341cc581804
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-04-29 18:46:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int i=0,j=0;
         vector<int>ans;
+        int i=0,j=0;
         deque<int>dq;
-        while(j<nums.size()){           
+        while(j<nums.size()){
             while(!dq.empty() && nums[j]>dq.back()){
                 dq.pop_back();
             }
@@ -16,9 +16,8 @@ public:
                 }
                 i++;
             }
-                
-                       
             j++;
+
         }
         return ans;
     }
