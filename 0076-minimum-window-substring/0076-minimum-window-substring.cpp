681b691424e559c01ee86d29bfdab5e7af4e107c
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-04-29 19:59:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int ind = -1, mini = INT_MAX;
-        unordered_map<char,int> mp;
-
-        for(auto i : t){
+        int i=0,j=0,c=0,mini=INT_MAX,ind=-1;
+        unordered_map<char,int>mp;
+        for(auto i:t){
             mp[i]++;
         }
-
-        int i = 0, j = 0, c = 0;
-
-        while(j < s.length()){
-            if(mp.find(s[j]) != mp.end()){
-                if(mp[s[j]] > 0){
+        while(j<s.length()){
+            if(mp.find(s[j])!=mp.end()){
+                if(mp[s[j]]>0){
                     c++;
                 }
-                mp[s[j]]--;
+                mp[s[j]]--;                
             }
-
-            while(c == t.length()){
-                if(j - i + 1 < mini){
-                    mini = j - i + 1;
-                    ind = i;
+            while(c==t.length()){
+                if(j-i+1<mini){
+                    mini=j-i+1;
+                    ind=i;
                 }
-
-                if(mp.find(s[i]) != mp.end()){
+                if(mp.find(s[i])!=mp.end()){
                     mp[s[i]]++;
-                    if(mp[s[i]] > 0){
+                    if(mp[s[i]]>0){
                         c--;
                     }
                 }
@@ -34,7 +28,6 @@ public:
             }
             j++;
         }
-
-        return (ind == -1) ? "" : s.substr(ind, mini);
+        return mini==INT_MAX?"":s.substr(ind,mini);
     }
 };
