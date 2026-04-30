15c41ff776d2c756f48fa957c2aeb972fcf5d3f7
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-04-30 21:59:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,20 +11,12 @@
  */
 class Solution {
 public:
-    int maxi=INT_MIN;
     int maxDepth(TreeNode* root) {
         if(root==nullptr){
             return 0;
         }
-        return traverse(root,0);
-    }
-    int traverse(TreeNode* root,int c) {
-        if(root==nullptr){
-            return INT_MIN;
-        }
-        if(root->left==nullptr && root->right==nullptr){
-            return c+1;
-        }
-        return max(traverse(root->left,c+1),traverse(root->right,c+1));
+        int left=maxDepth(root->left);
+        int right=maxDepth(root->right);
+        return 1+max(left,right);
     }
 };
