a0e62ce005cf2e5f13573498f4735fd734a95679
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-05-06 23:49:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    ListNode* find(ListNode*temp,int tar){
+    ListNode* find(ListNode* temp, int tar){
         int c=1;
         while(temp!=nullptr){
             if(c==tar){
@@ -22,24 +22,20 @@ public:
         return temp;
     }
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next || k == 0) return head;
         int c=1;
-        ListNode* start=head;
         ListNode* temp=head;
         while(temp->next!=nullptr){
             c++;
             temp=temp->next;
         }
-        
-        if(k%c==0){
+        if(c==k){
             return head;
         }
-        temp->next=head;
         k=k%c;
-        ListNode* newtail=find(head,c-k);
-        ListNode* newhead=newtail->next;
-        newtail->next=nullptr;
-        
+        temp->next=head;
+        ListNode* newnode=find(head,c-k);
+        ListNode* newhead=newnode->next;
+        newnode->next=nullptr;
         return newhead;
     }
 };
