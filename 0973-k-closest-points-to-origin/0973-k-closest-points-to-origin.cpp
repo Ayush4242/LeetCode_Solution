72fca041b3cb74c5393572e71d03ca5316d94fce
Ayush4242 <150381966+Ayush4242@users.noreply.github.com>
2026-07-08 14:42:45 +0530

    Time: 52 ms (53.56%), Space: 76.4 MB (42.58%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue< pair<int, vector<int>>,vector<pair<int, vector<int>>>,greater<pair<int, vector<int>>>> pq;
-        for(auto i:points){
-            int one=i[0];
-            int two=i[1];
-            int res=(one*one+two*two);
-            pq.push({res,i});
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {    
+        priority_queue<pair<int,pair<int,int>>>pq;
+        for(int i=0;i<points.size();i++){
+            int val=(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
+            pq.push({{val},{points[i][0],points[i][1]}});
         }
-        vector<vector<int>>res;
-        while(k>0){
-            res.push_back(pq.top().second);
+        while(pq.size()>k){
             pq.pop();
-            k--;
         }
-        return res;
+        vector<vector<int>>res1;
         
+        while(!pq.empty()){
+            vector<int>res;
+            int st=pq.top().second.first;
+            int sec=pq.top().second.second;
+            res.push_back(st);
+            res.push_back(sec);
+            pq.pop();
+            res1.push_back(res);
+        }
+        return res1;
     }
 };
